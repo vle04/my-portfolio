6b92204bfd6fe11f3d77892e363338ed72c014b6
vle04 <ngocvinale@gmail.com>
2025-07-10 14:37:11 -0400

    guh
    lint

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 // import { useState } from "react";
+import Image from "next/image";
 
 export default function ProfileCard() {
     // state for the profile image
@@ -10,10 +11,15 @@ export default function ProfileCard() {
         <div
             className="flex flex-row border border-gray-200 max-w-6xl max-h-1vh text-center p-8 bg-red-50"
         >
-            <img src={"/vina-avatar.png"} className="w-55"></img>
+            <Image
+                src={"/vina-avatar.png"}
+                alt="self-drawn avatar"
+                width={220}
+                height={200}
+            />
             <div>
-                <h1>hi, i&apos;'m vina</h1>
-                <text>i&apos;'m an aspiring software developer studing computer science at tufts university!</text>
+                <h1>hi, i&apos;m vina</h1>
+                <text>i&apos;m an aspiring software developer studing computer science at tufts university!</text>
                 {/* vina&apos;s portfolio, under construction haha */}
             </div>
         </div>
