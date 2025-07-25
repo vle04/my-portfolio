79b600e83683ff483861a5fa08b79970173c85b6
vle04 <ngocvinale@gmail.com>
2025-07-24 22:36:19 -0400

    started working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import ProfileCard from "@/components/ProfileCard";
+// import ProfileCard from "@/components/ProfileCard";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       {/* <ProfileCard/> */}
+      <Hero/>
     </main>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,25 @@
 "use client"
 
 import Image from "next/image";
+import BunnyAvatar from "@/public/images/bunny-avatar.png";
 
 export default function Hero() {
     return (
+        <div className="flex flex-row justify-center items-center gap-[70px]">
+            <Image
+                src={BunnyAvatar}
+                alt="vina head with a bunny hat"
+                width={250}
+                height={350}
+            />
 
+            {/* text container */}
+            <div>
+                <h1>hi, i'm vina</h1>
+                <p>i'm a junior at tufts university studying computer science!</p>
+                <p>currently a software developer @ freebites</p>
+                <p>passionate about the intersection of design and tech :D</p>
+            </div>
+        </div>
     );
 }
