b31026da67826d984b66be0fad4974dd19dfc0ff
vle04 <ngocvinale@gmail.com>
2025-07-31 19:17:54 -0400

    added temp icons

--- src/components/WindowSystem/Desktop.tsx
+++ src/components/WindowSystem/Desktop.tsx
@@ -7,6 +7,8 @@ import Window from "./WindowContainer";
 import AboutWindow from "./WindowContents/AboutWindow";
 import WorkWindow from "./WindowContents/WorkWindow";
 import PlayWindow from "./WindowContents/PlayWindow";
+import Icon from "@/public/file.svg";
+import Image from "next/image";
 
 export default function DesktopWindow() {
   const [windows, setWindows] = useState({
@@ -27,12 +29,28 @@ export default function DesktopWindow() {
 
   return (
     <>
-      <div className="absolute top-4 left-6 grid justify-items-start">
-        <button onClick={() => openWindow("about")}>
-            about me
+      <div className="absolute top-4 left-6 grid gap-4">
+        <button
+          onClick={() => openWindow("about")}
+          className="flex flex-col items-center gap-2 hover:bg-gray-200 p-2"
+        >
+          <Image src={Icon} alt="temporary icon" width={80} />
+          about me
         </button>
-        <button onClick={() => openWindow("work")}>work</button>
-        <button onClick={() => openWindow("play")}>play!</button>
+        <button
+          onClick={() => openWindow("work")}
+          className="flex flex-col items-center gap-2 hover:bg-gray-200 p-2"
+        >
+          <Image src={Icon} alt="temporary icon" width={80} />
+          work
+        </button>
+        <button
+          onClick={() => openWindow("play")}
+          className="flex flex-col items-center gap-2 hover:bg-gray-200 p-2"
+        >
+          <Image src={Icon} alt="temporary icon" width={80} />
+          play!
+        </button>
       </div>
 
       {windows.about && (
