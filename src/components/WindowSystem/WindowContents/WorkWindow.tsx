4cd7ede4a8c4ea38ea90404873ee86081388d0f1
vle04 <ngocvinale@gmail.com>
2025-08-30 18:26:44 -0400

    simple pagination

--- src/components/WindowSystem/WindowContents/WorkWindow.tsx
+++ src/components/WindowSystem/WindowContents/WorkWindow.tsx
@@ -10,7 +10,8 @@ import { portableTextComponents } from "@/sanity/lib/portableTextComponents";
 
 export default function WorkWindow() {
   const [projects, setProjects] = useState<Project[]>([]);
-  const [selectedProj, setSelectedProj] = useState<Project | null>(null);
+  const [selectedProj, setSelectedProj] = useState(1); // index of the current proj
+  const currProj = projects[selectedProj];
 
   // fetch projects
   useEffect(() => {
@@ -19,8 +20,6 @@ export default function WorkWindow() {
       // if (data.length > 0) setSelectedProj(data[0]); // select first proj by default
     });
   });
-
-  
 
   return (
     <div className="flex flex-row w-full h-115">
@@ -31,10 +30,10 @@ export default function WorkWindow() {
           <div
             key={proj._id}
             onClick={() => {
-              setSelectedProj(proj);
+              setSelectedProj(projects.indexOf(proj));
             }}
             className={`border-b-[3px] border-gray-300 px-8 py-4 hover:bg-gray-200 cursor-pointer ${
-              selectedProj?._id === proj._id ? "bg-gray-200" : ""
+              currProj?._id === proj._id ? "bg-gray-200" : ""
             }`}
           >
             <h3>{proj.title}</h3>
@@ -45,42 +44,61 @@ export default function WorkWindow() {
 
       {/* project info */}
       <div className="flex flex-col overflow-y-scroll px-8 py-4 min-w-[650px] w-full gap-4">
-        {selectedProj ? (
+        {currProj ? (
           <>
             <div className="flex flex-row items-center justify-between">
-              <h2>{selectedProj.title}</h2>
-              <p>{selectedProj.details}</p>
+              <h2>{currProj.title}</h2>
+              <p>{currProj.details}</p>
             </div>
 
             <div className="flex flex-col gap-2">
               <h4>overview</h4>
-              <p>{selectedProj.overview}</p>
+              <p>{currProj.overview}</p>
             </div>
 
             <div className="flex flex-col gap-2">
               <h4>tools</h4>
               <div className="flex flex-row flex-wrap gap-2">
-                {selectedProj.tools?.map((tool: string, i: number) => (
+                {currProj.tools?.map((tool: string, i: number) => (
                   <ToolTab key={i} text={tool} />
                 ))}
               </div>
             </div>
 
-            {/* <Image src={StockPhoto} alt="stock photo" className="w-full my-4" />
-            <p>probably some more text down here</p> */}
+            {/* project body */}
+            <PortableText
+              value={currProj.body}
+              components={portableTextComponents}
+            />
 
-            {/* <div className="prose"> */}
-              <PortableText value={selectedProj.body} components={portableTextComponents}/>
-            {/* </div> */}
+            {/* pagination */}
+            <div className="flex flex-row justify-between rounded bg-gray-200 p-4">
+              {/* back button */}
+              <p
+                onClick={() => {
+                  setSelectedProj(selectedProj - 1);
+                }}
+                className="hover:text-gray-300"
+              >
+                back
+              </p>
 
-            {/* pagination, make component for buttons*/}
-            <div className="flex flex-row justify-between">
-              <p className="hover:text-gray-300">back</p>
-              <p className="hover:text-gray-300">next</p>
+              {/* page number */}
+              <p>page {selectedProj + 1}</p>
+
+              {/* next button */}
+              <p
+                onClick={() => {
+                  setSelectedProj(selectedProj + 1);
+                }}
+                className="hover:text-gray-300"
+              >
+                next
+              </p>
             </div>
           </>
         ) : (
-          <p>select a project to view details</p>
+          <p>select a project to view details!</p>
         )}
       </div>
     </div>
